9b246a2a4b2621be7cc047207d9e2b8bd7cf1971
Victor Brito <victor-ebg@hotmail.com>
2020-11-25 10:10:15 -0400

    remove git config

--- src/models/Employee.ts
+++ src/models/Employee.ts
@@ -1,10 +1,5 @@
 export class Employee {
     id?: string;
     name?: string;
-
     lastName?: string;
 }
-
-
-git config --global user.email "victor-ebg@hotmail.com"
-git config --global user.name "Victor Brito"
